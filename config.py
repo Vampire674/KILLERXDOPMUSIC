2cd11cf09905807d01b24fec81799d8c7bd307dd
Vampire674 <vampirelegend5001@gmail.com>
2025-01-01 19:23:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,20 +5,20 @@ from dotenv import load_dotenv
 from pyrogram import filters
 
 load_dotenv()
-# ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
+# ━━━━━━━━━━━━━❖ ➥ 𝐊𝐈𝐋𝐋𝐄𝐑 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
 # Get this value from my.telegram.org/apps
 API_ID = int(getenv("API_ID", "23392712"))
 
-# ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
+# ━━━━━━━━━━━━━❖ ➥ 𝐊𝐈𝐋𝐋𝐄𝐑 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
 API_HASH = getenv("API_HASH", "7cb236b197b25c243fa83e7e0173d0e6")
 
-# ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
+# ━━━━━━━━━━━━━❖ ➥ 𝐊𝐈𝐋𝐋𝐄𝐑 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-# ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
+# ━━━━━━━━━━━━━❖ ➥ 𝐊𝐈𝐋𝐋𝐄𝐑 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
-# ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
+# ━━━━━━━━━━━━━❖ ➥ 𝐊𝐈𝐋𝐋𝐄𝐑 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 5400))
 
@@ -27,29 +27,29 @@ SONG_DOWNLOAD_DURATION = int(
 )
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", ""))
-# ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
-# Get this value from @L2R_KING on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "5820831398"))
-# ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
+# ━━━━━━━━━━━━━❖ ➥ 𝐊𝐈𝐋𝐋𝐄𝐑 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
+# Get this value from @llKILLER_OPll on Telegram by /id
+OWNER_ID = int(getenv("OWNER_ID", "7840042336"))
+# ━━━━━━━━━━━━━❖ ➥ 𝐊𝐈𝐋𝐋𝐄𝐑 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-# ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
+# ━━━━━━━━━━━━━❖ ➥ 𝐊𝐈𝐋𝐋𝐄𝐑 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BWFTIME/L2RMUSIC",
+    "https://github.com/Vampire674/KILLERXDOPMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
-# ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ALL_QUIZ_TAME")
-# ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BWF_MUSIC1")
-# ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
+# ━━━━━━━━━━━━━❖ ➥ 𝐊𝐈𝐋𝐋𝐄𝐑 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/KILLER_UPDATE")
+# ━━━━━━━━━━━━━❖ ➥ 𝐊𝐈𝐋𝐋𝐄𝐑 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/KILLER_UPDATE")
+# ━━━━━━━━━━━━━❖ ➥ 𝐊𝐈𝐋𝐋𝐄𝐑 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", "False"))
@@ -59,10 +59,10 @@ AUTO_SUGGESTION_TIME = int(
 # Get this credentials from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "bcfe26b0ebc3428882a0b5fb3e872473")
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "907c6a054c214005aeae1fd752273cc4")
-# ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
+# ━━━━━━━━━━━━━❖ ➥ 𝐊𝐈𝐋𝐋𝐄𝐑 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
-# ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
+# ━━━━━━━━━━━━━❖ ➥ 𝐊𝐈𝐋𝐋𝐄𝐑 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
 CLEANMODE_DELETE_MINS = int(
     getenv("CLEANMODE_MINS", "5"))
 # Telegram audio and video file size limit (in bytes)
@@ -70,14 +70,14 @@ TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 # Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
-# ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
+# ━━━━━━━━━━━━━❖ ➥ 𝐊𝐈𝐋𝐋𝐄𝐑 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
 # Get your pyrogram v2 session from @STRINGKINGBOT on Telegram
 STRING1 = getenv("STRING_SESSION", "")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
-# ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
+# ━━━━━━━━━━━━━❖ ➥ 𝐊𝐈𝐋𝐋𝐄𝐑 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
 
 BANNED_USERS = filters.user()
 adminlist = {}
@@ -91,10 +91,10 @@ clean = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/e576aa8308c49d945f433.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/6fxh5d.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/e576aa8308c49d945f433.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/6fxh5d.jpg"
 )
 PLAYLIST_IMG_URL = "https://telegra.ph/file/e576aa8308c49d945f433.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/e576aa8308c49d945f433.jpg"
